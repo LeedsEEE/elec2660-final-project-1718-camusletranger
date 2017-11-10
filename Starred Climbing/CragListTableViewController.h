e24a3f8a5f22084e2cd8a84bfb57aecbe35602a0
Joshua Marshall [ll13jrm] <ll13jrm@eentpc04.leeds.ac.uk>
2017-11-10 15:07:19 +0000

    Basic UI elements added with auto layout View hierarchy to be established

---
+++ Starred Climbing/CragListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CragListTableViewController.h
+//  Starred Climbing
+//
+//  Created by Joshua Marshall [ll13jrm] on 10/11/2017.
+//  Copyright © 2017 Joshua Marshall [ll13jrm]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CragListTableViewController : UITableViewController
+
+@end
